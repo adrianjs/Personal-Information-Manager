cd42b0f0b4e605eae1c4a55ff93937549a2baa75
adrst <adrste10@hotmail.com>
2017-09-27 15:38:53 +0200

    Made something work

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>This is my first React App!</p>
+            </div>
+        );
+    }
+};
